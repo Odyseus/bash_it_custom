9b2278fe787eda7ebfd7168b220b39cac8d2a633
Odyseus <venusyadonis@gmail.com>
2022-02-25 00:05:46 -0300

    Updated custom aliases

--- 999---custom.aliases.bash
+++ 999---custom.aliases.bash
@@ -80,6 +80,10 @@ alias youvideo='youtube-dl -o "$HOME/Videos/%(title)s.%(ext)s"'
 
 alias youaudio='youtube-dl -o "$HOME/Music/%(title)s.%(ext)s" -f "bestaudio[ext=m4a]"'
 
+alias youvid='yt-dlp -o "$HOME/Videos/%(title)s.%(ext)s"'
+
+alias youau='yt-dlp -o "$HOME/Music/%(title)s.%(ext)s" -f "bestaudio[ext=m4a]"'
+
 alias bashrc-reload='. ~/.bashrc'
 
 __gitio_func() {
